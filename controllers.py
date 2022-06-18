fee93c6c73dd6236aaf243f0f777499ee3182b6c
shion200 <usushio2002@gmail.com>
2022-06-18 14:49:28 +0900

    Create models

--- controllers.py
+++ controllers.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
+import db
+from models import User, Task
 
 app = FastAPI(
     title='FastAPIでつくるtoDoアプリケーション',
@@ -15,6 +17,11 @@ def index(request: Request):
     return{'Hello' : 'World'}
 
 def admin(request: Request):
+    user = db.session.query(User).filter(User.username == 'admin').first()
+    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+    db.session.close()
+    
     return templates.TemplateResponse('admin.html',
                                       {'request': request,
-                                      'username': 'admin'})
+                                      'user': user,
+                                      'task' : task})
--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-RDB_PATH = 'sqlite:///sb.sqlite3'
+RDB_PATH = 'sqlite:///db.sqlite3'
 ECHO_LOG = True
 
 engine = create_engine(
